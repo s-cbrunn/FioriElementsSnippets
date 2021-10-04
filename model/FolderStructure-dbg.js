7bc1b4902945db7ac3d84856762ce2094f7df2f1
s-cbrunn <spam_bc@web.de>
2021-10-04 16:58:56 +0200

    Github API

--- model/FolderStructure-dbg.js
+++ model/FolderStructure-dbg.js
@@ -8,23 +8,36 @@ sap.ui.define([
 
 		getJSONModel: function () {
 			this.oJsonModel = new JSONModel();
-			
+
 			return this.oJsonModel
 		},
 		getFolderStructure: function () {
 			this.folderStructure = {};
 			this.folderRequest = new XMLHttpRequest();
-			this.sendRequest('content/');
+			debugger;
+			if (window.location.hostname === "localhost") {
+				this.sendRequest('content/', '')
+			} else {
+				this.sendRequest('https://api.github.com/repos/s-cbrunn/FioriElementsSnippets/git/trees/b067e5ee58baafdfb6f4576eb4b0325f472c596f', '')
+			}
 
 		},
 
 		sendRequest: function (sPath, sModelPath) {
+			if (window.location.hostname === "localhost") {
+				this._sendRequestLocalHost(sPath, sModelPath);
+			} else {
+				this._sendRequestGitHub(sPath, sModelPath);
+			}
+		},
+
+		_sendRequestLocalHost: function (sPath, sModelPath) {
 
 			this.folderRequest.open('GET', sPath, true);
 			this.folderRequest.send();
 
 			this.folderRequest.onreadystatechange = function () {
-				this.aTreeStructure = [];
+				this._aTreeStructure = [];
 				if (this.folderRequest.readyState === 4) {
 
 					var element = document.createElement('html');
@@ -34,29 +47,67 @@ sap.ui.define([
 
 					for (var i = 2; i < directoryElements.length; i++) {
 
-						if (directoryElements[i].className === 'icon icon-directory') {
+						if (directoryElements[i].className === 'icon icon-directory') {	// File
 							debugger;
 							var sFolderName = directoryElements[i].title.substring(0, directoryElements[i].title.length - 1);
-							var sUrl = directoryElements[i].href.replace('http://','').replace(directoryElements[i].host,'');
+							var sUrl = directoryElements[i].href.replace('http://', '').replace(directoryElements[i].host, '');
 							var oFolderObject = {
 								text: sFolderName,
 								childUrl: sUrl,
 								nodes: [{ text: "", }]
 							};
-							this.aTreeStructure.push(oFolderObject);
+							this._aTreeStructure.push(oFolderObject);
 						} else if (directoryElements[i].className === 'icon icon icon-md icon-text' ||
-						           directoryElements[i].className === 'logo logo-img-1x') {
+							directoryElements[i].className === 'logo logo-img-1x') {	// Folder
 							var sFileName = directoryElements[i].title.substring(0, directoryElements[i].title.length);
 							var oFileObject = {
 								text: sFileName,
 							};
-							this.aTreeStructure.push(oFileObject);
+							this._aTreeStructure.push(oFileObject);
 						}
 					}
 
-					this.oJsonModel.setProperty(sModelPath ? sModelPath + "/nodes" : "/", this.aTreeStructure);
+					this.oJsonModel.setProperty(sModelPath ? sModelPath + "/nodes" : "/", this._aTreeStructure);
 
 
+				}
+			}.bind(this);
+
+		},
+
+
+		_sendRequestGitHub: function (sPath, sModelPath) {
+
+			this.folderRequest.open('GET', sPath, true);
+			this.folderRequest.send();
+
+			this.folderRequest.onreadystatechange = function () {
+				this._aTreeStructure = [];
+				if (this.folderRequest.readyState === 4) {
+
+
+					var oGitHubObject = JSON.parse(this.folderRequest.responseText);
+					var oGitHubObjectTree = oGitHubObject.tree;
+
+					for (var i = 0; i < oGitHubObjectTree.length; i++) {
+
+						if (oGitHubObjectTree[i].type === "tree") {  // Folder
+							var oFolderObject = {
+								text: oGitHubObjectTree[i].path,
+								childUrl: oGitHubObjectTree[i].url,
+								nodes: [{ text: "", }]
+							};
+							this._aTreeStructure.push(oFolderObject);
+						} else {								   // File
+							var oFileObject = {
+								text: oGitHubObjectTree[i].path,
+							};
+							this._aTreeStructure.push(oFileObject);
+						}
+
+					}
+
+					this.oJsonModel.setProperty(sModelPath ? sModelPath + "/nodes" : "/", this._aTreeStructure);
 				}
 			}.bind(this);
 
